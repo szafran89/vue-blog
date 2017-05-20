5774b50f40a99fadf751c7b8911302319c1952eb
Pawel Szafranski <pawel.szafranski@snow.dog>
2017-05-20 12:47:28 +0200

    Add Post component and instal contenful lib

---
+++ src/components/Posts.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div>
+    <h1>Posts</h1>
+    <ul class="list-group">
+      <li class="list-group-item"
+          v-for="post in posts">
+        {{ post }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'posts',
+    data() {
+      return {
+        posts: ['111', 222],
+      };
+    },
+  };
+</script>
+
+<style lang="css">
+
+</style>
